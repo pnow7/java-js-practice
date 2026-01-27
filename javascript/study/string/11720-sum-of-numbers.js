843b7f545f94360d0f008ae5ddcce3ca99994a8c
현재 <phyunjae7333@gmail.com>
2026-01-27 22:51:36 +0900

    [boj] split(""), map(Number), reduce((acc, cur) => ~~, 0) Fk z_z
    Signed-off-by: 현재 <phyunjae7333@gmail.com>

---
+++ javascript/study/string/11720-sum-of-numbers.js
@@ -0,0 +1,26 @@
+/*
+- 숫자의 합
+
+N개의 숫자가 공백 없이 쓰여있다. 
+이 숫자를 모두 합해서 출력하는 프로그램을 작성
+
+[입력]
+25
+7000000000000000000000000
+
+[출력]
+7
+*/
+
+const fs = require('fs');
+const input = fs.readFileSync("input.txt").toString().trim().split(/\s+/)
+
+const N = Number(input[0]);
+
+let sum = 0;
+for (let i = 1; i <= N; i++) {
+    let num = Number(input[1].slice(i - 1, i));
+    sum += num;
+}
+
+console.log(sum);
---
+++ javascript/study/string/upgrade/11720U-sum-of-numbers.js
@@ -0,0 +1,26 @@
+/*
+- 숫자의 합
+
+N개의 숫자가 공백 없이 쓰여있다. 
+이 숫자를 모두 합해서 출력하는 프로그램을 작성
+
+[입력]
+25
+7000000000000000000000000
+
+[출력]
+7
+*/
+
+const fs = require('fs');
+const input = fs.readFileSync("input.txt").toString().trim().split(/\s+/)
+
+// 배열로 쳐만들기
+const num = input[1].split("");
+const result = num.map(Number).reduce((acc, cur) => acc + cur, 0);
+const result2 = num.reduce((acc, cur) => {
+    return acc + Number(cur);
+}, 0);
+
+console.log(result);
+console.log(result2);
